70a41671e36ca5cab509ddba5bfc28b8250fba53
advenger95 <35837223+advenger95@users.noreply.github.com>
2019-04-21 14:27:28 +0800

    added nav elements

--- PortfolioProject/PortfolioProject/wwwroot/TS/tsscript.ts
+++ PortfolioProject/PortfolioProject/wwwroot/TS/tsscript.ts
@@ -1,5 +1,5 @@
 ﻿///<reference path="../node_modules/@types/jquery/jquery.d.ts" />
 
 $(document).ready(() => {
-    $(document.body).css('background-color', 'magenta');
+    $(document.body).css('background-color', 'blue');
 });

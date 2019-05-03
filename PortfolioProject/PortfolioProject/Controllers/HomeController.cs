4732ec2c8654cfa94ab3d76d291bd7d2b4f1d371
advenger95 <35837223+advenger95@users.noreply.github.com>
2019-05-03 16:25:37 +0800

    created a functioning modal and controller input for the contact form

--- PortfolioProject/PortfolioProject/Controllers/HomeController.cs
+++ PortfolioProject/PortfolioProject/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PortfolioProject.Models;
+using System.Net.Mail;
 
 namespace PortfolioProject.Controllers
 {
@@ -30,15 +31,57 @@ namespace PortfolioProject.Controllers
         {
             return View();
         }
+
+
+        [HttpGet]
         public IActionResult Contact()
         {
             return View();
         }
+        [HttpPost]
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Contact(ContacViewModal vm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msz = new MailMessage();
+                    msz.From = new MailAddress(vm.Email);//Email which you are getting 
+                                                         //from contact us page 
+                    msz.To.Add("greenfeld.adam@gmail.com");//Where mail will be sent 
+                    msz.Subject = vm.Subject;
+                    msz.Body = vm.Message;
+                    SmtpClient smtp = new SmtpClient();
+
+                    smtp.Host = "smtp.gmail.com";
+
+                    smtp.Port = 587;
+
+                    smtp.Credentials = new System.Net.NetworkCredential
+                    ("greenfeld.adam@gmail.com", "ModernMan1969");
+
+                    smtp.EnableSsl = true;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+
+                    smtp.Send(msz);
+
+                    ModelState.Clear();
+                    ViewBag.Message = "Thank you for Contacting me! I'll be with you shortly!!! ";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
+                }
+            }
+
+            return View();
+        }
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
